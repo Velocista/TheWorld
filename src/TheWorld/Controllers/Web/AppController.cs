7d0cf938a2ad98348b8ef07d5b146a8a89911840
ansross <ansross@VC-ITB-133-L>
2016-08-12 09:48:40 -0700

    The MVC 6 Module changes

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheWorld.Services;
+using TheWorld.ViewModels;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if(model.Email.Contains("aol.com"))
+            {
+                ModelState.AddModelError("Email", "We don't support AOL Addresses");  // This will assign the model error to the Email Field
+                ModelState.AddModelError("", "We don't support AOL Addresses");  //This will assign the error to the Model
+            }
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From the World", model.Message);
+
+                ViewBag.UserMessage = "Message Sent";
+
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
