feb0d818501715bb566acf3185e40da2ba41e0c6
ansross <ansross@VC-ITB-133-L>
2016-09-01 16:10:16 -0700

    Half of the Angular module

---
+++ src/TheWorld/wwwroot/js/app-trips.js
@@ -0,0 +1,10 @@
+﻿//app-trips.js
+(function () {
+    
+    "use strict";
+
+    //This defines the module because of the dependency parameter of type array []
+    //thus this is creating the Module
+    angular.module("app-trips", []);
+
+})();
---
+++ src/TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,35 @@
+﻿//tripsController.js
+(function () {
+
+    "use strict";
+
+    //This is referencing or getting the existing module 
+    //This returns an object so we can add our controller
+    angular.module("app-trips")
+    .controller("tripsController", tripsController);
+
+    function tripsController() {
+
+        //this represents the object that is returned from the controller
+        // the code is going to call  'new controller()', which will return a prototypical class
+        // which is obtained through the 'this' pointer
+
+        var vm = this;
+
+        vm.trips = [{
+            name: "US Trip",
+            created: new Date()
+        }, {
+            name: "World Trip",
+            created: new Date()
+        }];
+
+        vm.newTrip = {} ;
+
+        vm.addTrip = function () {
+            alert(vm.newTrip.name);
+        }
+
+    }
+
+})();
